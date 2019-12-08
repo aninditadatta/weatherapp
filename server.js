64b1977adaaf94ce53f61495073bf89c034ed017
aninditadatta <58663507+aninditadatta@users.noreply.github.com>
2019-12-08 22:18:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+
+const request = require('request');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', function (req, res) {
+   res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+
+let apiKey = 'e5738d4f15512fb995efa66524454a20';
+let city = req.body.city;
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  
+request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+
+})
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
